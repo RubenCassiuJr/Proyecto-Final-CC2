5c115bb2a34066b0e10be815450f6b974103bb73
Rubén <ruben.ajcu@galileo.edu>
2024-11-16 19:10:30 -0600

    Perfeccionamiento de las banderas.

--- ProcessScheduler.java
+++ ProcessScheduler.java
@@ -3,6 +3,9 @@
 			 ** Carnet: 21003655
 			 ** Seccion: B
 **/
+
+import javax.annotation.processing.SupportedOptions;
+
 /**
  * LA SIGUIENTE CLASE ES LA CLASE PRINCIPAL, EN ELLA IMPLEMENTAREMOS LAS BANDERAS
  * LAS POLÍTICAS, Y LAS LLAMADAS A LOS MÉTODOS
@@ -15,6 +18,7 @@ public class ProcessScheduler {
     public static void main(String[] args){
 
         String banderas = args[0];
+        String inputFileName = (args.length >= 2) ? args[1] : "";
 
         switch(banderas){
 
@@ -33,6 +37,21 @@ public class ProcessScheduler {
             case"-rr":
                 System.out.println("La política seleccionada es Round-Robin. ");
                 break; 
+            case"-help":
+                System.out.println("LA UTILIZACIÓN DE LAS BANDERAS ES LA SIGUIENTE: ");
+                System.out.println();
+                System.out.println("----> java ProcessScheduler -fcfs rango_tiempo_ingreso arith io cond loop  o");
+                System.out.println("----> java ProcessScheduler -lcfs rango_tiempo_ingreso arith io cond loop  o");
+                System.out.println("----> java ProcessScheduler -pp   rango_tiempo_ingreso arith io cond loop  o");
+                System.out.println("----> java ProcessScheduler -rr   rango_tiempo_ingreso arith io cond loop  quantum ");
+                System.out.println();
+                break; 
+
+            default:
+                System.out.println("Bandera no válida. Consulta la ayuda con '-help'.");
+
+                
+
         
             
 
